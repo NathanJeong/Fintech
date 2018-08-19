fa5043e011cf8c1843acb1073943bcab102ab316
nathanjeong <hyeonwonomics@gmail.com>
2018-08-19 20:19:27 +0900

    development 1 day practice how to login kiwoon stock service

--- login.py
+++ login.py
@@ -9,44 +9,49 @@ class MyWindow(QMainWindow):
         self.setWindowTitle("PyStock")
         self.setGeometry(300, 300, 300, 150)
 
-        label = QLabel('종목코드: ',self)
-        label.move(20,20)
-
-        
-
-        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1") # for using kiwoom class get instance from OpenAPI
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.kiwoom.dynamicCall("CommConnect()")
+
+        self.kiwoom.OnEventConnect.connect(self.OnEventConnect)
+        self.kiwoom.OnReceiveTrData.connect(self.OnReceiveTrData)
+        self.setupUI()
+
+    def setupUI(self):
+        label = QLabel('종목코드: ', self)
+        label.move(20, 20)
+
+        self.code_edit = QLineEdit(self)
+        self.code_edit.move(80, 20)
+        self.code_edit.setText("039490")
+
+        btn1 = QPushButton("조회", self)
+        btn1.move(190, 20)
+        btn1.clicked.connect(self.btn1_clicked)
 
         self.text_edit = QTextEdit(self)
         self.text_edit.setGeometry(10, 60, 280, 80)
         self.text_edit.setEnabled(False)
 
-        self.kiwoom.OnEventConnect.connect(self.event_connect)
-
-    def event_connect(self, err_code):
-        if err_code == 0:
-            self.text_edit.append("Login success")
-
-        btn1 = QPushButton("Login", self)
-        btn1.move(20,20)
-        btn1.clicked.connect(self.btn1_clicked)
-
-        btn2 = QPushButton("Check state", self)
-        btn2.move(20, 70)
-        btn2.clicked.connect(self.btn2_clicked)
-
     def btn1_clicked(self):
-        ret = set.kiwoom.dynamicCall("CommConnect()")
+        code = self.code_edit.text()
+        self.text_edit.append("종목코드: " + code)
+        self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
+        self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)", "Request1", "opt10001", 0, "0101")
 
-    def btn2_clicked(self):
-        if self.kiwoom.dynamicCall("GetConnectStat()") == 0:
-            self.statusBar().showMessage("Not connected")
-        else:
-            self.statusBar().showMessage("Connected")
+    def OnEventConnect(self, ErrCode):
+        if ErrCode == 0:
+            self.text_edit.append("로그인 성공")
+
+    def OnReceiveTrData(self, ScrNo, RQName, TrCode, RecordName, PrevNext, DataLength, ErrorCode, Message, SplmMsg):
+        if RQName == "Request1":
+            name = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", TrCode, "", RQName, 0, "종목명")
+            volume = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", TrCode, "", RQName, 0, "PER")
+
+            self.text_edit.append("종목명: " + name.strip())
+            self.text_edit.append("거래량: " + volume.strip())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = MyWindow()
-    myWindow.show()
+    mywindow = MyWindow()
+    mywindow.show()
     app.exec_()
-
