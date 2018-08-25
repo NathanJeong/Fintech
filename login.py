75f8ff9619d275611faf8be323dc5888740a05aa
nathanjeong <hyeonwonomics@gmail.com>
2018-08-25 19:02:18 +0900

    development 2 day practice how to login Kiwoom stock service analysis of each code from books

--- login.py
+++ login.py
@@ -16,7 +16,7 @@ class MyWindow(QMainWindow):
         self.kiwoom.OnReceiveTrData.connect(self.receive_trdata) # Tran 수신시 이벤트
 
         self.setWindowTitle("PyStock") # window title
-        self.setGeometry(300, 300, 300, 150) # window geometry
+        self.setGeometry(300, 500, 300, 150) # window geometry
 
         label = QLabel('종목코드: ', self) # label name
         label.move(20, 20) # label location
@@ -33,15 +33,27 @@ class MyWindow(QMainWindow):
         self.text_edit.setGeometry(10, 60, 280, 80) # text output location(x,y,width, height)
         self.text_edit.setEnabled(False) #
 
+
+
     def event_connect(self, err_code): # event_connect event 발생 시 처리하는 과정
         if err_code == 0: # OnEventConnect method는 로그인 성공시 0을 반환
             self.text_edit.append("로그인 성공")
         else:
             self.text_edit.append("로그인 실패, 아이디 혹은 비밀번호가 틀렸습니다.")
 
+    '''
+    1) SetInputValue 메서드를 사용해 TR 입력 값을 설정합니다. 
+    2) CommRqData 메서드를 사용해 TR을 서버로 송신합니다. 
+    3) 서버로부터 이벤트가 발생할 때까지 이벤트 루프를 사용해 대기합니다. 
+    4) CommGetData 메서드를 사용해 수신 데이터를 가져옵니다. 
+    '''
+
+    # btn1 event 처리 과정
     def btn1_clicked(self): # btn1 event 발생 시 처리하는 과정
         code = self.code_edit.text() # code_edit에 입력되는 값을 불러와서 code로 정의
         self.text_edit.append("종목코드: " + code) # text edit section에 종목코드:
+        account_num = self.kiwoom.dynamicCall("GetLoginInfo(QString)", ["ACCNO"])
+        self.text_edit.append("계좌번호: " + account_num.rstrip(';'))
 
         # SetInputValue : TR input value를 설정
         self.kiwoom.dynamicCall("SetInputValue(QString, QString)", "종목코드", code) # SetInputValue를 파이썬에서 사용할 수 있도록 호출
@@ -53,13 +65,17 @@ class MyWindow(QMainWindow):
         if rqname == "opt10001_req":
             # ComGetData 메서드를 사용해 수신데이터를 가져온다.
             name = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, 0, "종목명")
+            # name이라는 변수는 CommGetData 메서드를 통해 trcode, rqname을 송신받음.
             volume = self.kiwoom.dynamicCall("CommGetData(QString, QString, QString, int, QString)", trcode, "", rqname, 0, "거래량")
+            # volume이라는 변수는 CommGetData 메서드를 통해 trcode, rqname을 송신받음.
 
-            self.text_edit.append("종목명: " + name.strip())
-            self.text_edit.append("거래량: " + volume.strip())
+
+            self.text_edit.append("종목명: " + name.strip()) # text_edit에 종목명: name.strip()을 출력
+            # self.strip()은 좌우 공백을 모두 자르는 method
+            self.text_edit.append("거래량: " + volume.strip()) # text_edit에 거래량: volume.strip()을 출력
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = MyWindow()
-    myWindow.show()
+    myWindow = MyWindow() # myWindow class를 MyWindow() 생성자 클래스로 정의
+    myWindow.show() # myWindow를 개별 화면으로 출력
     app.exec_()
